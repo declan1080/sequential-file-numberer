d7e8ea6c6d966b2ac0b4c98b7ea6dd20e70bd620
DeclanE <declanemery6@gmail.com>
2023-11-15 20:14:06 +0000

    Implement safeguard against renaming the script

--- sequential-file-numberer.bat
+++ sequential-file-numberer.bat
@@ -13,8 +13,22 @@ ECHO.
 
 setlocal EnableDelayedExpansion
 
-REM Get the directory path from the user
-set /p dir=Enter the directory path: 
+REM Display the menu
+ECHO Select an option:
+ECHO 1. Rename files in current directory
+ECHO 2. Rename files in a specific directory
+set /p option=Enter option number: 
+
+REM Check the selected option and execute the corresponding code
+if !option! == 1 (
+    set "dir=%cd%"
+) else if !option! == 2 (
+    set /p dir=Enter the directory path: 
+) else (
+    ECHO Invalid option selected.
+    pause
+    exit /b
+)
 
 REM Get the prefix from the user
 set /p prefix=(Optional) Enter the prefix: 
@@ -27,11 +41,14 @@ for %%f in (%dir%\*) do (
     REM Get the extension of the file
     set "ext=%%~xf"
 
-    REM Rename the file with the prefix, the counter, and the original extension
-    ren "%%f" "!prefix!!counter!!ext!"
+    REM Check if the file is not the script file
+    if not "%%~nxf" == "sequential-file-numberer.bat" (
+        REM Rename the file with the prefix, the counter, and the original extension
+        ren "%%f" "!prefix!!counter!!ext!"
 
-    REM Increment the counter
-    set /a counter+=1
+        REM Increment the counter
+        set /a counter+=1
+    )
 )
 pause
 endlocal
