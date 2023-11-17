35fff23a449a651a664e4e216dcaafc1f9d3e06d
DeclanE <declanemery6@gmail.com>
2023-11-17 16:03:15 +0000

    File Type Filtering Feature.

--- sequential-file-numberer.bat
+++ sequential-file-numberer.bat
@@ -4,7 +4,7 @@ ECHO.
 ECHO ############################################################
 ECHO ##                                                        ## 
 ECHO ##            Sequential File Numberer                    ##
-ECHO ##            Version 1.0.0 15/11/2023                    ##
+ECHO ##            Version 1.0.1 16/11/2023                    ##
 ECHO ##                                                        ##
 ECHO ## https://github.com/declan1080/sequential-file-numberer ##
 ECHO ##                                                        ## 
@@ -48,11 +48,14 @@ if not exist "%dir%" (
 REM Get the prefix from the user
 set /p prefix=(Optional) Enter the prefix: 
 
+REM Get the file type filter from the user
+set /p fileTypeFilter=(Optional) Enter the file type filter (e.g. *.txt): 
+
 REM Initialize the counter
 set /a counter=1
 
 REM Loop through each file in the directory
-for %%f in ("%dir%\*") do (
+for %%f in ("%dir%\%fileTypeFilter%") do (
     REM Get the extension of the file
     set "ext=%%~xf"
 
